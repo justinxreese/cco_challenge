a94a9bc80464cf937c699f5c331f6f4261c15200
Justin Reese <justin.x.reese@gmail.com>
2012-04-10 21:27:43 -0400

    Adds logic for handling bad file names

--- stock.rb
+++ stock.rb
@@ -1,4 +1,14 @@
 require './lib/shelf_stocker.rb'
 
-stocker = ShelfStocker.new
+begin
+  products = File.new('dat/products.json') 
+  shelves = File.new('dat/shelves.json')
+rescue Exception => e
+  puts e
+  exit
+end
+
+stocker = ShelfStocker.new(products,shelves)
 stocker.fill_shelves
+
+# So that's what things would be like if I'd invented the fing-longer
